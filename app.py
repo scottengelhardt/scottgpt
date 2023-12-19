db5c1eb02e39263d306e18eb9ee456f5d6d12b90
Scott Engelhardt <scott@mindstamp.io>
2023-12-19 11:26:38 -0500

    spacing

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def main():
 
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-         if st.button("Open My Resume"):
+         if st.button(" Open My Resume "):
             webbrowser.open_new_tab(
                 "https://github.com/scottengelhardt/scottgpt/blob/dc87c1b12f559e12dfe6029e009eb4f7f9fb5ed8/resume.pdf"
             )
